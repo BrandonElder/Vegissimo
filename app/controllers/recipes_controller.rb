e5f4109396fb866417bc9fd6f0ac2f395549bdf4
EC2 Default User <ec2-user@ip-172-31-17-188.us-east-2.compute.internal>
2018-01-19 14:58:30 +0000

    errors helper  and tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,20 +1,23 @@
 class RecipesController < ApplicationController
-  
+  require 'tasks/recipe_errors'
+
   def show
-    recipe =
-    if params[:name]
-      Recipe.new(name: params[:name], edamam_id: params[:id])
-    else
-      Recipe.find_by_id(params[:id])
-    end
+    recipe = if params[:name]
+               Recipe.new(name: params[:name], edamam_id: params[:id])
+             else
+               Recipe.find_by_id(params[:id])
+             end
 
     if recipe
       @recipe = RecipesHelper.recipe_dto_from_api(recipe)
+      redirect_to root_path if RecipeErrors.api_limit?
     else
+      # can redirect back to user dashboard once implemented for favorites,
+      # user dashboard for saved recipes
       redirect_to action: 'show', id: 1, status: :not_found
     end
   end
-  
+
   def create
     @recipe = Recipe.create(name: params[:name], edamam_id: params[:id])
     redirect_to recipe_path(@recipe.id)
@@ -25,7 +28,7 @@ class RecipesController < ApplicationController
     @recipe.delete
     redirect_to root_path
   end
-  
+
   private
 
   def recipe_params
