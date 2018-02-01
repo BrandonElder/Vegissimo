d4ea426c6ed15d922cb06ff34a98bec257d9297e
EC2 Default User <ec2-user@ip-172-31-17-188.us-east-2.compute.internal>
2018-02-01 19:52:05 +0000

    search form styling, flex grid work, fixed tests, and more

--- spec/controllers/queries_controller_spec.rb
+++ spec/controllers/queries_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe QueriesController, type: :controller do
   describe 'queries#index action' do
     it 'should successfully show the page' do
-      get :index
+      get :home
       expect(response).to have_http_status(:success)
     end
   end
--- spec/helpers/queries_helper_spec.rb
+++ spec/helpers/queries_helper_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
-require 'query_result'
-require 'recipe_dto'
 file = File.read('spec/example1.json')
 data_hash = JSON.parse(file)
 
 RSpec.describe QueriesHelper, type: :helper do
   describe 'make_dto' do
     subject(:recipe) do
-      QueryResult.store_query_result(data_hash, 'pizza')
-      QueryResult.hits[0]
+      query = QueryResult.new(data_hash, '1234')
+      query.hits[0]
     end
+
     it 'creates a RecipeDto object from the recipe json data' do
       expect(recipe.make_dto.class).to eq(RecipeDto.new(recipe['recipe']).class)
     end
+
     it 'creates a RecipeDto object that has the correct values (testing label)' do
       expect(recipe.make_dto.label).to eq(RecipeDto.new(recipe['recipe']).label)
     end
