68a33a6c788dde04df654edf8725e28841c689c4
EC2 Default User <ec2-user@ip-172-31-17-188.us-east-2.compute.internal>
2018-02-07 03:53:26 +0000

    fixed failing test

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,27 +1,42 @@
 require 'rails_helper'
-require 'get_recipe'
-require 'recipe_dto'
-file = File.read('spec/example2.json')
-data_hash = JSON.parse(file)[0]
 
 RSpec.describe RecipesController, type: :controller do
+  let(:user1) { User.create!(email: 'example@gmail.com', password: 'bagels') }
+  let(:user2) { User.create!(email: 'example2@gmail.com', password: 'muffins') }
+  let(:eggplant) do
+  Recipe.create(user_id: user1.id,
+                name: 'Spicy Eggplant',
+                edamam_id:
+                'http://www.edamam.com/ontologies/edamam.owl#recipe_a53ef6c8495adcb9f2859b1e5d99e9ba')
+end
+
   describe 'show' do
-    let(:user) { User.create!(email: 'me@here.com', password: 'sdkjh59sda') }
-    subject(:eggplant) do
-      user.recipes.create(name: 'Spicy Eggplant', edamam_id:
-      'http://www.edamam.com/ontologies/edamam.owl#recipe_a53ef6c8495adcb9f2859b1e5d99e9ba')
-    end
     it 'returns a success status if the recipe is found' do
-      sign_in user
-      allow(RecipesHelper).to receive(:recipe_dto_from_api)
-        .with(eggplant).and_return(data_hash)
+      sign_in user1
+      allow(controller).to receive(:recipe_dto_from_api)
       get :show, params: { id: eggplant.id }
       expect(response).to have_http_status(:success)
     end
+
     it 'returns a 404 error if the recipe is not found' do
-      sign_in user
+      sign_in user1
+      allow(controller).to receive(:recipe_dto_from_api)
       get :show, params: { id: 'Blue' }
       expect(response).to have_http_status(:not_found)
+    end
+  end
+  describe 'destroy' do
+    it 'allows an authenticated user to destroy a recipe they created' do
+      sign_in user1
+      delete :destroy, params: { id: eggplant.id }
+      user1.recipes.reload
+      expect(user1.recipes).not_to include(eggplant)
+    end
+
+    it 'does not allow an unauthenticated user to destroy a recipe' do
+      delete :destroy, params: { id: eggplant.id }
+      user1.recipes.reload
+      expect(user1.recipes).to include(eggplant)
     end
   end
 end
